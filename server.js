51b474f39953e2ee075e9d36af8ec60b78e5127e
rajdeep2898 <rajdeep2898@gmail.com>
2020-05-30 23:20:50 +0530

    Prepared for deploy

--- server.js
+++ server.js
@@ -39,8 +39,10 @@ app.use("/api/profile", profile);
 app.use("/api/posts", post);
 
 //If in production
-if (process.env.Node_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
   app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
