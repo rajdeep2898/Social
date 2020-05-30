3c1b13d22bac190e97d01e8af7065d20627ad89c
rajdeep2898 <rajdeep2898@gmail.com>
2020-05-31 00:27:10 +0530

    ready to deploy

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const path = require("path");
-
 const users = require("./routers/api/users");
 const profile = require("./routers/api/profile");
 const post = require("./routers/api/posts");
